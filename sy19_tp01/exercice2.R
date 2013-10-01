15bf29237461b70c4660a5bd04ec8fef1043d113
jehu <antoine.hars@etu.utc.fr>
2013-10-01 23:19:17 +0200

    Ajout exo2 et 3

---
+++ sy19_tp01/exercice2.R
@@ -0,0 +1,10 @@
+# UV : SY19 - TP01
+# 2. Les données de mutations
+# Auteurs : Alice Ngwembou - Antoine Hars
+# Fichier : exercice2.R
+
+library(MASS)
+
+mutations = read.table("mutations2.txt", header = F, row.names = 1)
+
+
---
+++ sy19_tp01/exercice3.R
@@ -0,0 +1,9 @@
+# UV : SY19 - TP01
+# 3. Les données de distances entre aéroports
+# Auteurs : Alice Ngwembou - Antoine Hars
+# Fichier : exercice3.R
+
+library(MASS)
+
+airports = read.table("airports2.txt", header = F, row.names = 1)
+
