94b73e1fbdb157286efb9e86a5df1489af9f5bbe
jehu <antoine.hars@etu.utc.fr>
2013-10-08 23:53:29 +0200

    td2

--- sy19_tp01/exercice1.R
+++ sy19_tp01/exercice1.R
@@ -5,7 +5,7 @@
 
 library(MASS)
 
-x = matrix((8.5, 1.5, 3.5, 5.0, 2, 6.5, 9.5, 1.5, 8.5, 2.5, 3, 6.5, 9, 2.5, 2, 5.5), 2)
+x = matrix(c(8.5, 1.5, 3.5, 5.0, 2, 6.5, 9.5, 1.5, 8.5, 2.5, 3, 6.5, 9, 2.5, 2, 5.5), 2)
 x = t(x)
 
 # Première partie : ACP
@@ -13,13 +13,11 @@ cent = scale(x, center = TRUE, scale = FALSE)
 vari = (1/8) * t(cent) %*% cent
 diag = eigen(vari)
 
-# princomp(x)
-
 a = sum(diag$values)
 axe1 = diag$values[1] / a * 100
 axe2 = diag$values[2] / a * 100
 
-
+# ACP
 compx = princomp(x)
 biplot(compx, main = "Représentation des individus dans le premier plan factoriel", ylab = "axe 2", xlab = "axe 1"))
 abline(lty = 1, a = 0, b = 0, col = 1)
@@ -31,14 +29,44 @@ abline(lty = 1, a = 0, b = 1000000, col = 1)
 sumpca1 = diag$values[1]*t(compx$loadings[,1])
 sumpca2 = diag$values[2]*t(compx$loadings[,2])
 
-
 # Deuxième partie : MDS
-w1 = x %*% t(x)
+# 1)
+d = dist(x, method = "euclidean")
+
+# 2)
+w1 = cent %*% t(cent)
 
 id8 = diag(8)
-d = dist(x, method = "euclidean")
 distance <- as.matrix(d)
 un = matrix(rep(1, 64), 8)
 q8 = id8 - (1/8) * un
-w2 = -(1/2) * q8 %*% distance %*% q8
+w2 = -(1/2) * q8 %*% distance^2 %*% q8
+
+# 3)
+n = 1 / 8
+cent = scale(n * w1, center = TRUE, scale = FALSE)
+vari = (1/8) * t(cent) %*% cent
+diag = eigen(vari)
+# Il faut montrer que les valeurs propres sont positives ou nulles.
+
+
+# 4)
+dp = diag$vectors
+l = diag$values * id8
+
+# 5)
+aftd = cmdscale(d, 7)
+
+# 6)
+plot(aftd$points)
+# Faire la comparaison
+
+# 7)
+
+aftd <- function (d) {
+
+#	k =  dim(as.matrix(d))[1] - 1
+#	result = cmdscale(d, k)
+	return (result)
+}
 
--- sy19_tp01/exercice2.R
+++ sy19_tp01/exercice2.R
@@ -5,6 +5,29 @@
 
 library(MASS)
 
+# setwd("Z:/")
 mutations = read.table("mutations2.txt", header = F, row.names = 1)
 
+aftd <- function (d) {
+
+	dim_mat = as.matrix(d)
+	dimension = diag(dim(dim_mat))[1]
+	id = diag(dimension)
+
+	un = matrix(rep(1, dimension^2), dimension)
+	q = id - (1/dimension) * un
+	w = -(1/2) * q %*% dim_mat^2 %*% q
+
+	cent = scale((1 / dimension) * w, center = TRUE, scale = FALSE)
+	vari = (1 / dimension) * t(cent) %*% cent
+	diag = eigen(vari)
+
+	sort(diag$values, decreasing = TRUE)
+	sort(diag$vectors, decreasing = TRUE)
+
+	return (diag)
+}
+
+aftd1 = aftd(as.dist(mutations))
+aftd2 = cmdscale(as.dist(mutations), 14)
 
