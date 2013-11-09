8a9aaed9de2e6035dda0446a2b4c5d8b3b863e16
jehu <antoine.hars@etu.utc.fr>
2013-11-09 18:09:36 +0100

    exo1_1

--- sy19_tp02/exercice1.R
+++ sy19_tp02/exercice1.R
@@ -6,7 +6,7 @@
 library(MASS)
 library(cluster)
 
-# Chargement des donnees, selection des variables quantitatives et normalisation 
+# Chargement des données, sélection des variables quantitatives et normalisation 
 data(iris)
 donnees <- NULL
 donnees$num <- iris[,c(1:4)]
@@ -17,43 +17,53 @@ distmat = dist(donnees$num, method = "euclidean")
 # Variance des données
 vari = var(donnees$num)
 # Nombre d'individus
-dim1 = dim(donnees$num)
-# Nombre de critères
-dim(donnees$num)
+dim = dim(donnees$num)
 
 # Question 1
 # http://www.grappa.univ-lille3.fr/~ppreux/ensg/miashs/fouilleDeDonneesII/tp/k-moyennes/
 
-k2 =  kmeans(donnees$num,2)
-plot(donnees$num, col = c('red', 'blue')[k2$cluster])
+cl2 =  kmeans(donnees$num, 2)$cluster
+png(file = "plots/plot_exo1_kmeans_2.png")
+plot(donnees$num, col = c('red', 'blue')[cl2])
+dev.off()
+png(file = "plots/clusplot_exo1_kmeans_2.png")
+clusplot(donnees$num, cl2, color = TRUE, shade = TRUE, labels = 2, main = "Clusplot iris K = 2")
+dev.off()
 
-k3 =  kmeans(donnees$num,3)
-plot(donnees$num, col = c('red', 'blue', 'green')[k3$cluster])
+cl3 =  kmeans(donnees$num, 3)$cluster
+png(file = "plots/plot_exo1_kmeans_2.png")
+plot(donnees$num, col = c('red', 'blue', 'green')[cl3])
+dev.off()
+png(file = "plots/clusplot_exo1_kmeans_3.png")
+clusplot(donnees$num, cl3, color = TRUE, shade = TRUE, labels = 2, main = "Clusplot iris K = 3")
+dev.off()
 
-k4 =  kmeans(donnees$num,4)
-plot(donnees$num, col = c('red', 'blue', 'green', 'black')[k4$cluster])
-
+cl4 =  kmeans(donnees$num, 4)$cluster
+png(file = "plots/plot_exo1_kmeans_2.png")
+plot(donnees$num, col = c('red', 'blue', 'green', 'black')[cl4])
+dev.off()
+png(file = "plots/clusplot_exo1_kmeans_4.png")
+clusplot(donnees$num, cl4, color = TRUE, shade = TRUE, labels = 2, main = "Clusplot iris K = 4")
+dev.off()
 
 # Question 2
-
 tab = list()
-for (n in 1:100)
-{
+for (n in 1:100) {
 	k3 = kmeans(donnees$num,3)
-	inertie = sum(k3$withinss)/150
+	inertie = sum(k3$withinss) / 150
 
-tab = list()
-}   
+	tab = list()
+}
 
 # Question 3
+for (K in 2:5) {
 
-for (K in 2:5)
-{
-	for (n in 1:100)
-	{
+	for (n in 1:100) {
+
 		kK = kmeans(donnees$num,K)
-		inertie = sum(kK$withinss)/150
+		inertie = sum(kK$withinss) / 150
 	}
 }
 
 # Question 4
+
